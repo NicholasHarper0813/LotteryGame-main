16d3034b8762be9f9ba248ba24b8e543437d4bdc
nicholas harper <nicholasharper000@gmail.com>
2024-12-12 22:19:13 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'; 
-import Lottery from './Lottery';
 import Ball from "./Ball"; 
+import Lottery from './Lottery';
 import "./App.css";
 
-class App extends Component {
+class App extends Component 
+{
   render() 
   { 
   return (
