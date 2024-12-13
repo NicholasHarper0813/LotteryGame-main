63fb58ec6b988ba9b65300e5c693a2d9e1cd539c
nicholas harper <nicholasharper000@gmail.com>
2024-12-12 19:48:54 -0800

    Update Lottery.js

--- src/Lottery.js
+++ src/Lottery.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react'
+import './Lottery.css';
 import Ball from './Ball'; 
-import './Lottery.css'; 
 
-class Lottery extends Component {
+class Lottery extends Component 
+{
     static defaultProps = {
         title: 'Lotto',
         numBalls: 6,
         maxNum: 40
     }
-    constructor(props){
+    constructor(props)
+    {
         super(props); 
         this.state = { nums: Array.from({length: this.props.numBalls}) };
         this.handleClick = this.handleClick.bind(this);
     }
-    generate() {
+    generate() 
+    {
         this.setState(curState => ({
             nums: curState.nums.map(
                 n => Math.floor(Math.random() * this.props.maxNum) +1
             )
         }));
     }
-    handleClick(){
+    handleClick()
+    {
         this.generate();
     }
-    render() {
+    render() 
+    {
         return (
         <section className="Lottery">
         <h1>{this.props.title}</h1>
